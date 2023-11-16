6fd5d99b952f829c8bcc51b6851f95b5e5ccdaa1
hyejikng <hyeji8842@gmail.com>
2023-11-16 15:15:21 +0900

    making a motion component draggable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 
 const Wrapper = styled.div`
   display: flex;
@@ -20,15 +21,39 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(225, 225, 225, 0.2);
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
 const boxVariants = {
   hover: { scale: 2, rotate: 90 },
   click: { borderRadius: '100%', scale: 1 },
+  drag: { backgroundColor: 'rgb(46, 204, 113)', transition: { duration: 10 } },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileTap="click" whileHover="hover"></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          whileDrag="drag"
+          variants={boxVariants}
+          whileTap="click"
+          whileHover="hover"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
